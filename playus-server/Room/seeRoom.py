6ddfd10f21d5812782d323c4701a25c1c6b5a595
insung <inh3011@gmail.com>
2021-09-30 16:26:44 +0900

    seeRoom fetch

--- playus-server/Room/seeRoom.py
+++ playus-server/Room/seeRoom.py
@@ -1,23 +1,29 @@
 from logging import fatal
-import os, pymysql, json, datetime, bcrypt, jwt
+import os
+import pymysql
+import json
+import datetime
+import bcrypt
+import jwt
 from flask import request
 from flask_restx import Resource, Api, Namespace
 from dotenv import load_dotenv
 
-load_dotenv() # `.env`파일 불러옴
+load_dotenv()  # `.env`파일 불러옴
 
 db = pymysql.connect(host=os.getenv('MYSQL_HOST'),
-                    port=int(os.getenv('MYSQL_PORT')),
-                    user=os.getenv('MYSQL_USER'),
-                    passwd=os.getenv('MYSQL_PASSWORD'),
-                    db=os.getenv('MYSQL_DATABASE'),
-                    charset=os.getenv('MYSQL_CHARSET'),
-                    cursorclass=pymysql.cursors.DictCursor)\
+                     port=int(os.getenv('MYSQL_PORT')),
+                     user=os.getenv('MYSQL_USER'),
+                     passwd=os.getenv('MYSQL_PASSWORD'),
+                     db=os.getenv('MYSQL_DATABASE'),
+                     charset=os.getenv('MYSQL_CHARSET'),
+                     cursorclass=pymysql.cursors.DictCursor)\
 
 seeRoom = Namespace(
     name='seeRoom',
     description='Room API'
 )
+
 
 @seeRoom.route('/<string:user_name>')
 class Room(Resource):
@@ -25,7 +31,7 @@ class Room(Resource):
         '''방 리스트'''
 
         base = db.cursor()
-        sql = f'select user_sport, user_place from user\
+        sql = f'select user_sport, user_place from User\
                 where user_name = "{user_name}";'
         base.execute(sql)
         mark = base.fetchall()
@@ -34,19 +40,19 @@ class Room(Resource):
         # 값 받아서 저장
         if mark:
             for i in mark:
-                user_sport = i['user_sport'] # 종목
-                user_place = i['user_place'] # 장소
-        else :
+                user_sport = i['user_sport']  # 종목
+                user_place = i['user_place']  # 장소
+        else:
             user_sport = False
             user_place = False
 
-        if user_sport :
+        if user_sport:
             sport = True
-        else :
+        else:
             sport = False
-        if user_place :
+        if user_place:
             place = True
-        else :
+        else:
             place = False
 
         if sport and place:
@@ -70,18 +76,16 @@ class Room(Resource):
                 on u.room_no = r.room_no\
                 where r.room_place = "{user_place}"\
                 group by r.room_no having count(*);'
-        else :
+        else:
             sql = f'select r.room_no, r.room_title, r.room_place, r.room_time, r.room_total, u.user_name, COUNT(*) as room_user\
                 from Room as r\
                 right outer join Room_user as u\
                 on u.room_no = r.room_no\
                 group by r.room_no having count(*);'
-                
+
         base = db.cursor()
         base.execute(sql)
         data = base.fetchall()
         for i in data:
             i['room_time'] = str(i['room_time'])
-        return {'Rooms' : data}
-
-        
+        return {'Rooms': data}
--- playus-web/src/screens/Home.js
+++ playus-web/src/screens/Home.js
@@ -1,7 +1,20 @@
+import { useEffect } from "react";
 import BottomTabs from "../components/main/BottomTabs";
 import Header from "../components/main/Header";
 
 function Home() {
+  const me = localStorage.getItem("LOGIN");
+  const seeRoom = async (me) => {
+    const list = await fetch(`http://localhost:5000/seeRooms/${me}`).then(
+      (res) => res.json()
+    );
+    console.log(list);
+    return list;
+  };
+
+  useEffect((me) => {
+    seeRoom();
+  }, []);
   return (
     <div>
       <Header />
